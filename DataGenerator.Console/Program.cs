63b0e94033b3875ffcd78e9edb3ad2719e081d23
Nikolai Fedorov <nikolayfedorovv@yandex.ru>
2023-07-24 23:04:08 +0300

    Add new male first names and female first names

--- DataGenerator.Console/Program.cs
+++ DataGenerator.Console/Program.cs
@@ -1,12 +1,7 @@
 ﻿
-using DataGenerator.DataStore;
 
-//string[] maleLastNames = { "Федоров", "Анискин", "Пендеров", "Петухов", "Олухов", "Кипелов", "Кинчев", "Горшенев", "Летов", "Холстини", "Гоголь" };
 
-//DataItemJsonFileContext context = new(DataItemName.FemaleLastNames);
-//await context.AddNew(maleLastNames);
-
-var result = await DataGenerator.Data.Generate("3 firstname lastname birthdate year pet");
+var result = await DataGenerator.Data.Generate("100 firstname lastname birthdate year pet");
 
 
 Console.WriteLine(result);
